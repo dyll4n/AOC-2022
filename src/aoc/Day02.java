7dc8fc7ac0425ee34f95e716c95aa563b1e64ef8
jameshc2001 <jameshcrofts@gmail.com>
2024-07-25 19:44:51 +0100

    Refactor input parsing for Day 2

--- src/aoc/Day02.java
+++ src/aoc/Day02.java
@@ -1,42 +1,39 @@
 package aoc;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Day02 {
-    public enum Move { ROCK, PAPER, SCISSORS;}
+    public enum Move {
+        ROCK, PAPER, SCISSORS;
+
+        public static Move fromChar(char input) {
+            return switch (input) {
+                case 'A', 'X' -> Move.ROCK;
+                case 'B', 'Y' -> Move.PAPER;
+                case 'C', 'Z' -> Move.SCISSORS;
+                default -> throw new RuntimeException("Unexpected character for move: " + input);
+            };
+        }
+    }
+
     public record Strategy(Move opponentsMove, Move yourMove) {} //https://www.baeldung.com/java-record-keyword
 
 
     public static List<Strategy> getListOfStrategies(String input) {
         List<Strategy> strategies = new ArrayList<>();
         String[] moves = input.split("\\n");
-        Move yours = null, theirs = null;
 
-        for(int i = 0; i < moves.length; i++){
-            String strategy = moves[i].trim();
-
-            for(int j = 0; j < strategy.length(); j++){
-                if(j == 0) theirs = getMove(String.valueOf(strategy.charAt(j)));
-                else yours = getMove(String.valueOf(strategy.charAt(j)));
-            }
-            strategies.add(new Strategy(theirs, yours));
+        for (String move : moves) {
+            String strategy = move.trim().replaceAll(" ", "");
+            strategies.add(
+                    new Strategy(
+                            Move.fromChar(strategy.charAt(0)),
+                            Move.fromChar(strategy.charAt(1))
+                    )
+            );
         }
 
         return strategies;
-    }
-
-    public static Move getMove(String input) {
-        if(input.equals("A") || input.equals("X")){
-            return Move.ROCK;
-        } else if (input.equals("B") || input.equals("Y")) {
-            return Move.PAPER;
-        }else if (input.equals("C") || input.equals("Z")){
-            return Move.SCISSORS;
-        }
-        else {
-            return null;
-        }
     }
 }
